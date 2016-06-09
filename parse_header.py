3bfb992a1c9a5c0cf0432065dd3ddd6ae29addca
Abby Lambert <absterninja@gmail.com>
2016-06-09 11:35:07 -0700

    script for creating a list of shape objects from a test file, debugged

--- parse_header.py
+++ parse_header.py
@@ -1,36 +1,312 @@
 #!/usr/bin/python
 
 import numpy as np
+from shape_lib import *
+import math
 
 filename = 'test.txt'
 fo = open(filename, 'r+')
+ncol = 0
+nrows = 0
+cell_size = 0
+no_data = 0
 
-def getHeaderData(data):
-    ind = data.find("\t")
-    data = data[(ind+1):-1]
-    return float(data)
+#function for grabbing data from a line of the header
+def get_header_data(data):
+	ind = data.find("\t")
+	data = data[(ind+1):-1]
+	return float(data)
 
-def writeHeaderData():
-    col = fo.readline()
-    row = fo.readline()
-    cellsize = fo.readline()
-    nodata = fo.readline()
+#stores all of the header data
+def write_header_data():
+	col = fo.readline()
+	row = fo.readline()
+	cellsize = fo.readline()
+	nodata = fo.readline()
+    
+	ncol = get_header_data(col)
+	nrows = get_header_data(row)
+	cell_size = get_header_data(cellsize)
+	no_data = get_header_data(nodata)
 
-    fn = open('test_new.txt', 'w')
+#grabs all data between <BEGIN SHAPES> and <END SHAPES> tags
+def get_shape_data():
+	data = fo.read()	
+	ind = data.find("<")
+	data = data[(ind+15):]
+	ind = data.find("<")
+	data = data[:-13]
+	return data
 
-    fn.write(str(getHeaderData(col)) + "\n")
-    fn.write(str(getHeaderData(row)) + "\n")
-    fn.write(str(getHeaderData(cellsize)) + "\n")
-    fn.write(str(getHeaderData(nodata)) + "\n")
+write_header_data()
+shapes = get_shape_data()
 
-def getShapeData():
-    data = fo.read()
-    ind = data.find("<")
-    data = data[(ind+15):]
-    ind = data.find("<")
-    data = data[:-13]
-    fn = open('shape_data.txt', 'w')
-    fn.write(data)
+#function to shorten shape maker code
+def read_arg(shape):
+	ind = shape.find(",")
+	if(ind != -1):
+		clip = shape[:ind]
+	else:
+		ind = shape.find(")")
+		clip = shape[:ind]
+	return clip
+	
+def move_string(key, string):
+	ind = string.find(key)
+	string = string[(ind+1):]
+	return string
 
-writeHeaderData()
-getShapeData()
+#uses parse shape information to make shape objects
+def rect_maker(shape):
+	x = 0
+	y = 0
+	lx = 0
+	ly = 0
+	rot = 0
+
+	shape = move_string("(", shape)
+
+	x = int(read_arg(shape))
+	shape = move_string(" ", shape)
+	
+	y = int(read_arg(shape))
+	shape = move_string(" ", shape)
+	
+	lx = int(read_arg(shape))
+	shape = move_string(" ", shape)
+
+	ly = int(read_arg(shape))
+	shape = move_string(" ", shape)
+
+	rot = math.radians(float(read_arg(shape)))
+
+	rect = Rectangle(x, y, lx, ly, rot)
+	return rect
+
+def square_maker(shape):
+	x = 0
+	y = 0
+	l = 0
+	rot = 0
+
+	shape = move_string("(",shape)
+
+	x = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	y = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	l = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	rot = math.radians(float(read_arg(shape)))
+
+	square = Square(x, y, l, rot)
+	return square
+	
+
+def ellipse_maker(shape):
+	x = 0
+	y = 0
+	lx = 0
+	ly = 0
+	rot = 0
+
+	shape = move_string("(", shape)
+
+	x = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	y = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	lx = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	ly = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	rot = math.radians(float(read_arg(shape)))
+
+	ellipse = Ellipse(x, y, lx, ly, rot)
+	return ellipse
+
+
+def circle_maker(shape):
+	x = 0
+	y = 0
+	l = 0
+	rot = 0
+
+	shape = move_string("(", shape)
+
+	x = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	y = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	l = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	rot = math.radians(float(read_arg(shape)))
+
+	circle = Circle(x, y, l, rot)
+	return circle
+
+
+def rect3d_maker(shape):
+	x = 0
+	y = 0
+	z = 0
+	lx = 0
+	ly = 0
+	lz = 0
+	rot = 0
+
+	shape = move_string("(", shape)
+
+	x = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	y = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	z = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	lx = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	ly = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	lz = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	rot = math.radians(float(read_arg(shape)))
+
+	rect3d = Rectangle3D(x, y, z, lx, ly, lz, rot)
+	return rect3d
+
+
+def square3d_maker(shape):
+	x = 0
+	y = 0
+	z = 0
+	l = 0
+	rot = 0
+	
+	shape = move_string("(", shape)
+
+	x = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	y = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	z = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	l = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	rot = math.radians(float(read_arg(shape)))
+
+	square3d = Square3D(x, y, z, l, rot)
+	return square3d
+
+def ellipse3d_maker(shape):
+	x = 0
+	y = 0
+	z = 0
+	lx = 0
+	ly = 0
+	lz = 0
+	rot = 0
+	
+	shape = move_string("(", shape)
+
+	x = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	y = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	z = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	lx = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	ly = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	lz = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	rot = math.radians(float(read_arg(shape)))
+
+	ellipse3d = Ellipse3D(x, y, z, lx, ly, lz, rot)
+	return ellipse3d
+
+
+def circle3d_maker(shape):
+	x = 0
+	y = 0
+	z = 0
+	l = 0
+	rot = 0
+
+	shape = move_string("(", shape)
+
+	x = int(read_arg(shape))
+	shape = move_string(" ", shape)
+	
+	y = int(read_arg(shape))
+	shape = move_string(" ",shape)
+	
+	z = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	l = int(read_arg(shape))
+	shape = move_string(" ",shape)
+
+	rot = math.radians(float(read_arg(shape)))
+
+	circle3d = Circle3D(x, y, z, l, rot)
+	return circle3d
+
+#parses the shape information and creates objects for each
+def parse_shapes(shapes):
+	shape_list = []
+	while(shapes.find("\n") != -1):
+		ind = shapes.find("\n")
+		line = shapes[:ind]
+		if(line.find("rect") != -1):
+			if(line.find("3d") != -1):
+				shape_list.append(rect3d_maker(line))
+			else:
+				shape_list.append(rect_maker(line))
+		elif(line.find("ellipse") != -1):
+			if(line.find("3d") != -1):
+				shape_list.append(ellipse3d_maker(line))
+			else:
+				shape_list.append(ellipse_maker(line))
+		elif(line.find("square") != -1):
+			if(line.find("3d") != -1):
+				shape_list.append(square3d_maker(line))
+			else:
+				shape_list.append(square_maker(line))
+		elif(line.find("circle") != -1):
+			if(line.find("3d") != -1):
+				shape_list.append(circle3d_maker(line))
+			else:
+				shape_list.append(circle_maker(line))
+		shapes = shapes[(ind+1):]
+
+	return shape_list
+
+
+print parse_shapes(shapes)
--- shape_lib.py
+++ shape_lib.py
@@ -1,25 +1,25 @@
 class Abstract2D(object):
-        def __init__(self, x, y, dx, dy, rot):
+        def __init__(self, x, y, lx, ly, rot):
                 self.x = x
                 self.y = y
-                self.dx = dx
-                self.dy = dy
+                self.lx = lx
+                self.ly = ly
                 self.rot = rot
 class Abstract3D(object):
-	def __init__(self, x, y, z, dx, dy, dz, rot):
+	def __init__(self, x, y, z, lx, ly, lz, rot):
 		self.x = x
 		self.y = y
 		self.z = z
-		self.dx = dx
-		self.dy = dy
-		self.dz = dz
+		self.lx = lx
+		self.ly = ly
+		self.lz = lz
 		self.rot = rot
 class Rectangle(Abstract2D):
-        def __init__(self, x, y, dx, dy, rot):
-                Abstract2D.__init__(self, x, y, dx, dy, rot)
+        def __init__(self, x, y, lx, ly, rot):
+                Abstract2D.__init__(self, x, y, lx, ly, rot)
 class Ellipse(Abstract2D):
-	def __init__(self, x, y, dx, dy, rot):
-		Abstract2D.__init__(self, x, y, dx, dy, rot)
+	def __init__(self, x, y, lx, ly, rot):
+		Abstract2D.__init__(self, x, y, lx, ly, rot)
 class Square(Rectangle):
         def __init__(self, x, y, l, rot):
                 Rectangle.__init__(self, x, y, l, l, rot)
@@ -27,15 +27,15 @@ class Circle(Ellipse):
 	def __init__(self, x, y, l, rot):
 		Ellipse.__init__(self, x, y, l, l, rot)
 class Rectangle3D(Abstract3D):
-	def __init__(self, x, y, z, dx, dy, dz, rot):
-		Abstract3D.__init__(self, x, y, z, dx, dy, dz, rot)
-class Ellipse3D(Abstrarct3D):
-	def __init__(self, x, y, z, dx, dy, dz, rot):
-		Abstract3D.__init__(self, x, y, x, dx, dy, dz, rot)
+	def __init__(self, x, y, z, lx, ly, lz, rot):
+		Abstract3D.__init__(self, x, y, z, lx, ly, lz, rot)
+class Ellipse3D(Abstract3D):
+	def __init__(self, x, y, z, lx, ly, lz, rot):
+		Abstract3D.__init__(self, x, y, x, lx, ly, lz, rot)
 class Square3D(Rectangle3D):
 	def __init__(self, x, y, z, l, rot):
 		Rectangle3D.__init__(self, x, y, z, l, l, l, rot)
 class Circle3D(Ellipse3D):
 	def __init__(self, x, y, z, l, rot):
-		Ellipse3D).__init__(self, x, y, z, l, l, l, rot)
+		Ellipse3D.__init__(self, x, y, z, l, l, l, rot)
 
